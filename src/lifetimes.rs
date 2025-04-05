91f9b71e53553b0b0e3b92057d2b16867a662efa
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-05 21:24:36 +0000

    lifetimes

---
+++ src/lifetimes.rs
@@ -0,0 +1,87 @@
+// Rust can’t guarantee that the reference you return (&str) will live as long as both input references.
+// If one of the inputs dies early (goes out of scope),
+// your return value might point to invalid memory — and Rust hates that.
+
+// this one is the error code
+fn longest(s1: &str, s2: &str) -> &str {
+    // generic lifetime annotation
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
+fn main() {
+    let long_str;
+    let s1 = String::from("hello");
+    {
+        let s2 = String::from("Weorld");
+        long_str = longest(&s1, &s2);
+    }
+
+    println!("longest string is {}", long_str);
+}
+
+// this code is fine
+// "Both s1 and s2 must live at least as long as 'a, and the return value will live as long as 'a too."
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    // generic lifetime annotation
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
+fn main() {
+    let long_str;
+    let s1 = String::from("hello");
+    let s2 = String::from("Weorld");
+    long_str = longest(&s1, &s2);
+
+    println!("longest string is {}", long_str);
+}
+// “Trust me, these borrows all live at least as long as 'a, so returning one is safe.”
+
+// struct with lifetiemes
+// "name is a reference that must live at least as long as lifetime 'a"
+struct User<'a> {
+    name: &'a str,
+    age: i8,
+}
+
+fn main() {
+    let name = String::from("chaitanya");
+    let user = User {
+        name: &name,
+        age: 21,
+    };
+    // if name goes out scope, user should also go out of scope
+
+    println!("name is {} and age is {}", user.name, user.age);
+}
+
+// lifetimes with generics
+use std::fmt::Display;
+fn longest<'a, T>(s1: &'a str, s2: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    // generic lifetime annotation
+    println!("announcment! {ann}");
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
+fn main() {
+    let long_str;
+    let s1 = String::from("hello");
+    let s2 = String::from("Weorld");
+    long_str = longest(&s1, &s2, String::from("Hello world"));
+
+    println!("longest string is {}", long_str);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,22 @@
-// traits are abstract classes that either have just the declaration of a function\
-// or maybe default definition also that any struct can implement
-
-trait Summary {
-    fn summarise(&self) -> String {
-        return String::from("Hi there from summarise");
+use std::fmt::Display;
+fn longest<'a, T>(s1: &'a str, s2: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    // generic lifetime annotation
+    println!("announcment! {ann}");
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
     }
 }
-
-trait Fix {
-    fn fixes(&self) -> String {
-        return String::from("Hi there from fix");
-    }
-}
-
-struct User {
-    name: String,
-    age: i16,
-}
-
-impl Summary for User {}
-impl Fix for User {}
 
 fn main() {
-    let user = User {
-        name: String::from("chaitanya"),
-        age: 21,
-    };
+    let long_str;
+    let s1 = String::from("hello");
+    let s2 = String::from("Weorld");
+    long_str = longest(&s1, &s2, String::from("Hello world"));
 
-    println!("{} is {} years old", user.name, user.age);
-    println!("{}", user.summarise());
-    println!("{}", user.fixes());
-
-    // calling the notify function
-    notify(&user);
-
-    // error: the trait bound `String: Summary` is not satisfied
-    // notify(&String::from("hello"));
-
-    notification(user);
-}
-
-// traits as parameters
-fn notify(u: &impl Summary) {
-    // this means that the notify function will take any struct as arguement that
-    // implements the Summary trait
-    // we have specified the trait bound for the input of this fuction
-    // now we cannot input any struct to the  function that does not implement the Summary trait
-
-    println!("this is from notify function: {}", u.summarise());
-}
-// this is one way to specify the trait bound
-
-// the following is the better and more used way
-fn notification<T: Summary + Fix>(u: T) {
-    // this means that the input can be generic but it must implement Summary and fix trait
-    // no other argument is allowed for this function
-
-    println!("this is from notification: {}", u.summarise());
-    println!("this is from notification: {}", u.fixes());
+    println!("longest string is {}", long_str);
 }
